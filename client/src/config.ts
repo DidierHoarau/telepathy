5be53361e23d00870ea582bee630bbd704ab182d
Didier Hoarau <didier.hoarau.dev@gmail.com>
2018-10-22 17:52:42 +0800

    Add basic mechanism for auto-update

--- client/src/config.ts
+++ client/src/config.ts
@@ -2,8 +2,11 @@ import * as fse from 'fs-extra';
 
 class Config {
   //
-  public static readonly VERSION: string = '2018.10';
-  public static readonly CONFIG_FILE: string = '/etc/telepathy/config.json';
+  public static readonly CLIENT_VERSION_FILE: string = '/opt/telepathy/version-client';
+  public static readonly CONFIG_FILE: string = '/etc/telepathy/config-client.json';
+  public CLIENT_ARCH: string = 'telepathy-client-linux-x64';
+  public CLIENT_VERSION: string = 'undefined';
+  public UPDATE_AUTO: boolean = false;
   public UPDATE_URL_INFO: string = 'undefined';
   public UPDATE_URL_BINARY: string = 'undefined';
 
@@ -12,14 +15,22 @@ class Config {
   }
 
   public async reload(): Promise<void> {
-    const content = JSON.parse(await fse.readFile(Config.CONFIG_FILE, 'utf8'));
+    const content = await fse.readJson(Config.CONFIG_FILE);
     const setIfSet = field => {
       if (content[field]) {
         this[field] = content[field];
       }
     };
+    setIfSet('UPDATE_AUTO');
     setIfSet('UPDATE_URL_INFO');
     setIfSet('UPDATE_URL_BINARY');
+    try {
+      this.CLIENT_VERSION = (await fse.readFile(Config.CLIENT_VERSION_FILE))
+        .toString()
+        .split('\n')[0];
+    } catch (err) {
+      this.CLIENT_VERSION = 'undefined';
+    }
   }
 }
 
