9fbabbc32b1e172d342b057783fb30d375b90fae
Didier Hoarau <didier.hoarau.dev@gmail.com>
2018-10-24 07:27:14 +0800

    Install to systemctl and rename client to agent

--- server/src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { config } from '../config';
-import { exampleGetRouter } from './example-get';
-import { examplePostRouter } from './example-post';
-import { ExpressWrapper } from './utils/express-wrapper';
-
-export const router = ExpressWrapper.createRouter();
-
-router.use(`${config.API_PATH}/example/`, exampleGetRouter);
-router.use(`${config.API_PATH}/example/`, examplePostRouter);
