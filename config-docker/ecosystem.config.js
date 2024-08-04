c1f292d29193f8bf448f9d7fd3c66c16c4171c1f
Didier Hoarau <4222718+DidierHoarau@users.noreply.github.com>
2024-08-04 15:48:26 +0800

    WIP

--- config-docker/ecosystem.config.js
+++ config-docker/ecosystem.config.js
@@ -12,7 +12,6 @@ module.exports = {
       name: "server",
       cwd: "/opt/app/telepathy-server",
       script: "dist/app.js",
-      autorestart: false,
       env: {
         NODE_ENV: "production",
         TELEPATHY_DATA_DIR: "/data",
@@ -22,7 +21,6 @@ module.exports = {
       name: "agent",
       cwd: "/opt/app/telepathy-agent",
       script: "dist/app.js",
-      autorestart: false,
       env: {
         NODE_ENV: "production",
       },
